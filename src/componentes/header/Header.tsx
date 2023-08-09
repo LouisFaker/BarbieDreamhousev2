57e2f8be8bce2a6585d6d590876999274eb7d1de
Louis <le389568@gmail.com>
2023-08-09 09:52:03 -0400

    footer finalizado

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -6,20 +6,24 @@ export default function Header() {
   return (
     <header>
       <div className="logo">
-        <img src="./public/logo-barbie.png" alt="Logo da Barbie" className="logo-img" />
+        <img
+          src="./public/logo-barbie.png"
+          alt="Logo da Barbie"
+          className="logo-img"
+        />
         <span className="logo-text">ㅤ</span>
       </div>
       <nav>
         <ul>
           <li>
             <Link texto="Home" />
-            <Link texto="Home" />
+            <Link texto="imagenes" />
           </li>
           <li>
-            <Link texto="Home" />
+            <Link texto="Sobre" />
           </li>
           <li>
-            <Link texto="Home" />
+            <Link texto="Contacto" />
           </li>
           <li></li>
         </ul>
